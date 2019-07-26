86964533d91fc6e9118cb196ee5a4040ed6a9a61
Tyler Lawson <lawsont2@msu.edu>
2019-07-26 12:13:25 -0400

    stuffs and things

--- helpers.py
+++ helpers.py
@@ -1,5 +1,26 @@
 from json import dumps
+from yaml import Loader, load
+from pathlib import Path
+
 
 def pretty_print_dict(D):
 	print(dumps(D, indent=1))
-	
+
+
+def get_trucks():
+	return load("")
+
+
+def get_proj_dir():
+    my_path = str(Path.resolve(Path(__file__)))
+    return my_path[:my_path.index(__name__)]
+
+
+def get_vars(target):
+  # Get config file variables
+  path = get_proj_dir()
+  file = f"{path}trucks.yaml"
+  open(file)
+  result = load(open(file), Loader=Loader)[target]
+
+  return result
