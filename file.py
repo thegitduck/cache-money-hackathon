89fc82e25a6dbe2acc9bdd1bf360b7e7041f7a4c
Tyler Lawson <lawsont2@msu.edu>
2019-07-26 11:43:26 -0400

    api call works

--- file.py
+++ file.py
@@ -1,7 +1,10 @@
 import requests
 import facebook
+from helpers import *
 
 graph = facebook.GraphAPI(access_token="EAAfCEcXC3tQBABkfqXSIp2NSkIX6ZAra0wZBbDjzgYud2B4ldktM3dShjCzA7ikAPLYhZAvatdcwt5HffEqIrBb12J9B5whLZCgDreJ0wawj0PMH9leQqRT9k5fFOZCeGmbu4NKrIkVhuYvPtHmjfjdQ4XwqeCklSTZCue9PZAigxJbhP6u8ucFNjmTKWLY6GZAAWMuctdkd0AZDZD", version="2.12")
 
-location = graph.get_object(id='me', fields='location')
-print(location)
+places = graph.get_objects(ids=['NotARealFoodTruck'], fields='name,location')
+
+# location = graph.get_object(id='me', fields='location')
+pretty_print_dict(places)
---
+++ helpers.py
@@ -0,0 +1,5 @@
+from json import dumps
+
+def pretty_print_dict(D):
+	print(dumps(D, indent=1))
+	
